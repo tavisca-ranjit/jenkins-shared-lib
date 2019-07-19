3dcf1f59ae5570e7a79d1ab0bebd7f8ac0f95d4d
codeSonic <mjoshi@tavisca.com>
2019-07-19 11:23:06 +0530

    added a demo pipeline

---
+++ vars/demoPipeline.groovy
@@ -0,0 +1,69 @@
+def call(Map config){
+pipeline{
+
+	agent none
+
+	stages{
+		stage('Build'){
+		
+			agent {
+				kubernetes {
+				  cloud 'kubernetes-qa'
+				  label 'dynamicslavek8sdotnetbuild'
+				  defaultContainer 'dynamicslavedotnetbuild'
+				  yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+  podtype: jenkinsdynamimcslavedotnetbuild
+spec:
+  containers:
+  - name: dynamicslavedotnetbuild
+    image: mcr.microsoft.com/dotnet/core/sdk:2.1
+    command:
+    - cat
+    tty: true
+"""
+				}
+			}
+			steps{
+				
+				echo "Building application"
+				sh 'dotnet --version'
+				echo config.key1
+				sh 'sleep 1'
+			}
+		}
+
+		stage('Docker build image'){
+			agent {
+				kubernetes {
+				  cloud 'kubernetes-qatstg'
+				  label 'dynamicslavek8sdockerbuild'
+				  defaultContainer 'dynamicslavedockerbuild'
+				  yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+  podtype: jenkinsdynamimcslavedockerbuild
+spec:
+  containers:
+  - name: dynamicslavedockerbuild
+    image: docker
+    command:
+    - cat
+    tty: true
+"""
+				}
+			}
+			steps{
+				sh 'docker --version'
+				echo config.key2
+				sh 'sleep 10'
+			}
+		}
+	}
+}
+}
